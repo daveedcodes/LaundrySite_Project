f98ea50e8a6577aa993b0344d805108a6968531a
daveedcodes <94625057+daveedcodes@users.noreply.github.com>
2022-12-12 23:35:45 +0100

    first update

---
+++ pack/laundaryCare.js
@@ -0,0 +1,139 @@
+function faqdisplay(){
+    var displayC = document.getElementById("da")
+    if(displayC.style.display=="none"){
+        displayC.style.display="block";
+        
+    }
+    else {
+        displayC.style.display="none";
+    }
+}/*funcntion for faq drop button one*/
+function faqdisplayb(){
+    var displayC = document.getElementById("db")
+    if(displayC.style.display=="none"){
+        displayC.style.display="block";
+    }
+    else {
+        displayC.style.display="none";
+    }
+}/*funcntion for faq drop button two*/
+function faqdisplayc(){
+    var displayC = document.getElementById("dc")
+    if(displayC.style.display=="none"){
+        displayC.style.display="block";
+    }
+    else {
+        displayC.style.display="none";
+    }
+}/*funcntion for faq drop button third*/
+function faqdisplayd(){
+    var displayC = document.getElementById("dd")
+    if(displayC.style.display=="none"){
+        displayC.style.display="block";
+    }
+    else {
+        displayC.style.display="none";
+    }
+}/*funcntion for faq drop button fourth*/
+/*reveal function*/
+
+
+/*faq buttons*/
+
+
+function faqdisplayaa(){
+    var display = document.getElementById("a")
+    if(display.style.display=="none"){
+        display.style.display="block";
+        
+    }
+    else {
+        display.style.display="none";
+    }}
+  
+
+function faqdisplaybb(){
+var display = document.getElementById("b")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplaycc(){
+var display = document.getElementById("c")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplaydd(){
+var display = document.getElementById("d")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplayee(){
+var display = document.getElementById("e")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplayff(){
+var display = document.getElementById("f")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplaygg(){
+var display = document.getElementById("g")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplayhh(){
+var display = document.getElementById("h")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplayii(){
+var display = document.getElementById("i")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";
+}}
+function faqdisplayjj(){
+var display = document.getElementById("j")
+if(display.style.display=="none"){
+display.style.display="block";
+
+}
+else {
+display.style.display="none";  }}
+
+
+
+
+ 
